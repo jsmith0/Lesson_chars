4d60376a0b2fbc5438a649656b732e7a0a3cee9b
js456 <justins4@comcast.net>
2016-09-28 10:33:31 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        //can only hold 1 character
+        String difference = "a";
+        //can hold a ton of stuff
+        char a = difference.charAt(0);
+        System.out.println(a);
+        //the program does not see them as the same, a character is not a string
+
+        //ASCII (american standard code for information interchange) = the keyboard
+        //the keys we hit gets transferred in numbers, registers as a decimal, goes to binary
+        // A=65, a = 97 (KEY TO KNOW)
+        
+    }
+}
